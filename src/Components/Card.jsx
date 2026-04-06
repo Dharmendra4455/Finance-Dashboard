99abd1455e0c31ee1f3990d08d65ccfa6eb0c69c
Dharmendra Patel <dharemendra@mixinstechnology.onmicrosoft.com>
2026-04-06 08:27:10 +0530

    minor card changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -27,7 +27,7 @@ const{data ,setdata} =useContext(dataapi)
              </div>
           </div>
 
-          <div className='px-12 shrink-0 sm:px-3 md:px-6 lg:px-18 py-5  flex items-center gap-3  rounded-md text-xl text-zinc-50' style={{ backgroundColor: 'red' }}>
+          <div className='px-12 shrink-0 sm:px-2 md:px-4 lg:px-16 py-5  flex items-center gap-3  rounded-md text-xl text-zinc-50' style={{ backgroundColor: 'red' }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-banknote-arrow-down-icon lucide-banknote-arrow-down"><path d="M12 18H4a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5"/><path d="m16 19 3 3 3-3"/><path d="M18 12h.01"/><path d="M19 16v6"/><path d="M6 12h.01"/><circle cx="12" cy="12" r="2"/></svg>
               <div className="wrapper">
               <div className="text">Monthly Expenses</div>
